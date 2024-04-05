b59b9b75722f4b9fac78b0a4de2a078e842d35dd
irobertcostin <irobertcostin@gmail.com>
2024-04-05 21:01:01 +0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import cors from "cors"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cors())
-  // app.enableCors({
-  //   "origin": "https://effective-media-agency.netlify.app/",
-  //   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   "preflightContinue": false,
-  //   "optionsSuccessStatus": 204
-  // })
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "https://effective-media-agency.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+    credentials: true
+  })
 
   await app.listen(3020);
 }
