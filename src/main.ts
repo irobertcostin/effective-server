d37e13d2361f3af63eb6ad560f4eff840f6d46ba
irobertcostin <irobertcostin@gmail.com>
2024-04-08 23:28:04 +0300

    implemented search function

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3020
   // const port = 3020
   app.enableCors({
-    origin: ["https://effective-media-agency.netlify.app/", "https://effective-media-agency.netlify.app", "http://localhost:3000", "https://effective-media-blog.netlify.app", "https://effective-media-blog.netlify.app/", "https://effectivemedia.ro", "https://www.effectivemedia.ro"],
+    origin: ["https://effective-media-agency.netlify.app/", "https://effective-media-agency.netlify.app", "http://localhost:3000", "http://localhost:3001", "https://effective-media-blog.netlify.app", "https://effective-media-blog.netlify.app/", "https://effectivemedia.ro", "https://www.effectivemedia.ro"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Origin', 'XMLHttpRequest', 'X-Requested-With', 'Accept', 'Authorization'],
     exposedHeaders: ['Authorization'],
