cba63073450d35f74f8bab035af81764b622b76c
irobertcostin <irobertcostin@gmail.com>
2024-04-18 06:50:12 +0300

    CRUD operations authGuard

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -3,12 +3,13 @@ import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsSchema } from './schemas/clients.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [ClientsService],
   controllers: [ClientsController],
   imports: [
-    ClientsModule,
+    UsersModule,
     MongooseModule.forFeature([{ name: "Clients", schema: ClientsSchema }])
   ]
 })
